e9be48b810d2f88e0146ff51382ad03227f8f91f
Taillook <jousoku@gmail.com>
2017-09-30 22:48:28 +0900

    [Add] HomeTableView

--- Twift/Twift/HomeViewController.swift
+++ Twift/Twift/HomeViewController.swift
@@ -13,12 +13,29 @@ import OAuthSwift
 class HomeViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
+        setUI()
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func setUI() {
+        tableView.estimatedRowHeight = 110
+        tableView.rowHeight = UITableViewAutomaticDimension
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tweetCell", for: indexPath)
+        return cell
+    }
+    
+    override func tableView(_ table: UITableView,didSelectRowAt indexPath: IndexPath) {
+        print("tapped")
     }
 }
